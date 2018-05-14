ff7d3c75cb053d5dc2790be841a502b329bac1cf
k0sen <caladien12@gmail.com>
2018-05-14 20:45:52 +0300

    improved fighter build (fix #3)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Fighter from "./Fighter.js";
+import ImprovedFighter from "./ImprovedFighter.js";
 
 let fighter = new Fighter("Tesst", 232323, 232323);
+let fighter2 = new ImprovedFighter("Tesst22", 1233, 1231);
 
-fighter.setDamage(123);
+fighter2.setDamage(123);
 
-document.write(`${fighter.health}<br>`);
+document.write(`${fighter2.health}<br>`);
