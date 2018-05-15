469d428fbf0ac7e4eff1ee2cda8191fe15063859
k0sen <caladien12@gmail.com>
2018-05-15 21:05:43 +0300

    code prettify

--- src/fight.js
+++ src/fight.js
@@ -4,23 +4,25 @@ export default function fight(fighter, improvedFighter, ...point) {
   let i = 0;
   let k = 0;
 
+  console.log(`>>> ${fighter.name} vs ${improvedFighter.name} <<<`);
+
   while (fighter.health > 0 && improvedFighter.health > 0 && i < points.length) {
-    if (k === 0) {
+    if (k % 2 === 0) {
       fighter.hit(improvedFighter, points[i]);
       k++;
       continue;
     }
 
     improvedFighter.doubleHit(fighter, points[i]);
-    k = 0;
+    k++;
     i++;
   }
 
   if (fighter.health < 0) {
-    return `Figher "${improvedFighter.name}" wins. Gratz`;
+    console.log(`Figher "${improvedFighter.name}" wins. Gratz \n`);
   } else if (improvedFighter.health < 0) {
-    return `Figher "${fighter.name}" wins. Gratz`;
+    console.log(`Figher "${fighter.name}" wins. Gratz \n`);
+  } else {
+    console.log(`Tie between ${fighter.name} vs ${improvedFighter.name}. All participant fought well \n`);
   }
-
-  return `Tie. All participant fought well`;
 }
